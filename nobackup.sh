14a5ca719c6dfb840e7138615d942dff760621f6
Lancelot Normand <lancelotnormand@outlook.com>
2024-04-06 19:05:56 -0400

    fix nobackup

--- nobackup.sh
+++ nobackup.sh
@@ -1,3 +1,3 @@
 #to add the cuda drivers to path when cuda is installed in /nobackup like in bk-gpu-1
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export PATH=/nobackup/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/nobackup/usr/local/cuda/lib64:$LD_LIBRARY_PATH
