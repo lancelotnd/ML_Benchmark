79a91a1747a131fca40b594840e1a95c7e185282
Lancelot Normand <lancelotnormand@outlook.com>
2024-04-07 12:47:30 -0400

    add data volume to docker script

--- tf_docker.sh
+++ tf_docker.sh
@@ -1,2 +1,2 @@
-docker run --gpus all -it -w /tensorflow -v $PWD:/mnt -e HOST_PERMS="$(id -u):$(id -g)" \
+docker run --gpus all -it -w /tensorflow -v /data:/data -v $PWD:/mnt -e HOST_PERMS="$(id -u):$(id -g)" \
     tensorflow/tensorflow:devel-gpu bash
