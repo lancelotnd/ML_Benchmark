1ca7a67751bd6ebe2fb51970426fed15b289f68f
Lancelot Normand <lancelotnormand@outlook.com>
2024-04-15 14:35:36 -0400

    run all reduce

--- torch/run.py
+++ torch/run.py
@@ -3,8 +3,22 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
-def run(rank,size):
-    """ Non Blocking point to point communication """
+
+"""Blocking point-to-point communication."""
+def run_blocking_p2p(rank, size):
+    tensor = torch.zeros(1)
+    if rank == 0:
+        tensor += 1
+        # Send the tensor to process 1
+        dist.send(tensor=tensor, dst=1)
+    else:
+        # Receive tensor from process 0
+        dist.recv(tensor=tensor, src=0)
+    print('Rank ', rank, ' has data ', tensor[0])
+
+
+""" Non Blocking point to point communication """
+def run_non_blocking(rank,size):
     tensor = torch.zeros(1)
     req = None
     if rank == 0:
@@ -16,6 +30,14 @@ def run(rank,size):
     req.wait()
     print("Rank", rank, 'has data', tensor[0])
 
+
+""" All-Reduce example."""
+def run_all_reduce(rank,size):
+    """ Simple collective communication. """
+    group = dist.new_group([0, 1])
+    tensor = torch.ones(1)
+    dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
+    print('Rank ', rank, ' has data ', tensor[0])
 
 def init_process(rank, size, fn, backend='gloo'):
     """ Init the dist env """
@@ -30,7 +52,7 @@ if __name__ == "__main__":
     processes = []
     mp.set_start_method("spawn")
     for rank in range(size):
-        p = mp.Process(target=init_process, args=(rank,size,run))
+        p = mp.Process(target=init_process, args=(rank,size,run_all_reduce))
         p.start()
         processes.append(p)
     
