364b8dbdbe85ed85a4ebb2c7d44fc8f1fb46eeb6
Lancelot Normand <lancelotnormand@outlook.com>
2024-04-15 15:56:27 -0400

    add time to run mnist script

--- torch/run_mnist.sh
+++ torch/run_mnist.sh
@@ -6,6 +6,6 @@
 for i in {1..6}
 do
     echo "Running with nproc_per_node=$i"
-    torchrun --nnodes=1 --nproc_per_node=$i --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=127.0.0.1:29400 elastic_mnist.py 2>&1 | tee -a run_mnist_logs_$i.txt
+    time torchrun --nnodes=1 --nproc_per_node=$i --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=127.0.0.1:29400 elastic_mnist.py 2>&1 | tee -a run_mnist_logs_$i.txt
     echo "Completed run with nproc_per_node=$i"
 done
